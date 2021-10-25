9c328ecc8737ee7c98e5c828e05fb68ac1e0606a
Jahnavi <Jahnavi@LAPTOP-LAKVACQC>
2021-10-25 18:05:54 +0530

    worker and threadpool

---
+++ DXC/src/threads/WorkerThread.java
@@ -0,0 +1,32 @@
+package threads;
+
+public class WorkerThread  implements Runnable {
+      String message;
+      
+      
+	public WorkerThread(String msg) {	
+		message = msg;
+	}
+
+	@Override
+	public void run() {
+		System.out.println("thread name  is"+Thread.currentThread().getName());
+		System.out.println(" i am working on --"+message);
+		processMessage();
+		System.out.println("i am done with"+message+" thread name is "+Thread.currentThread().getName());
+	}
+
+	private void processMessage() {
+		try {
+	    	Thread.sleep(200);
+	}catch (InterruptedException e) {
+		//TODO Auto-generated catch block
+	    e.printStackTrace();
+	}
+	
+	
+	}
+	
+
+
+}
