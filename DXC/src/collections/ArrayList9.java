bed725d1e5dcf3f814a7fc3aab816a753937df67
Jahnavi <Jahnavi@LAPTOP-LAKVACQC>
2021-10-26 15:20:04 +0530

    Arraylinkedlist

---
+++ DXC/src/collections/ArrayList9.java
@@ -0,0 +1,24 @@
+package collections;
+
+import java.util.*;
+
+public class ArrayList9 {
+	public static void main(String args[]) {
+		ArrayList<String> al=new ArrayList<String>();
+		al.add("janu");
+		al.add("banu");
+		al.add("rethu");
+		ArrayList<String> al2=new ArrayList<String>();
+		al2.add("janu");
+		al2.add("apple");
+		al2.add("rethu");
+		
+		al2.retainAll(al2);
+		
+		System.out.println("iterating the elements after retaining the elements of al2");
+		Iterator itr=al.iterator();
+		while(itr.hasNext()) {
+			System.out.println(itr.next());
+		}
+	}
+}
