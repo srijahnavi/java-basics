08c43d1fb7bc35bd55111c3cac665dc1216b96b8
Jahnavi <Jahnavi@LAPTOP-LAKVACQC>
2021-10-26 12:28:43 +0530

    arraylist

---
+++ DXC/src/collections/DemoArrayList.java
@@ -0,0 +1,15 @@
+package collections;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DemoArrayList {
+	
+	public static void main(String[] args) {
+		List<String> names = new  ArrayList<String>();
+		names.add("Sweety");
+		//names.add(30);
+		System.out.println(names);
+	}
+
+}
