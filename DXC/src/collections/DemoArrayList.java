555b680da284d2d8788ce693759733933468295d
Jahnavi <Jahnavi@LAPTOP-LAKVACQC>
2021-10-26 12:43:05 +0530

    Iterator

--- DXC/src/collections/DemoArrayList.java
+++ DXC/src/collections/DemoArrayList.java
@@ -1,6 +1,7 @@
 package collections;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class DemoArrayList {
@@ -8,8 +9,18 @@ public class DemoArrayList {
 	public static void main(String[] args) {
 		List<String> names = new  ArrayList<String>();
 		names.add("Sweety");
+		names.add("Hotty");
+		
 		//names.add(30);
 		System.out.println(names);
+		/**
+		 * for(String name : names) { //for each name in names 
+		 * System.out.println("the name is "+name);
+		 */
+		Iterator itr=names.iterator();
+		while(itr.hasNext()) {
+		System.out.println(itr.next());
+		}
 	}
 
 }
