e2c27bf885be2a60cc4584a0747ea7000dfc9b79
Jahnavi <Jahnavi@LAPTOP-LAKVACQC>
2021-10-28 12:49:33 +0530

    Demogenericmethods

---
+++ DXC/src/generics/DemoGenericMethod.java
@@ -0,0 +1,20 @@
+package generics;
+
+public class DemoGenericMethod {
+	
+	public static <E> void printArray(E[] elements) {
+		for(E element  : elements) {
+			System.out.println(element);
+		}
+		System.out.println();
+	}
+	
+	public static void main(String[] args) {
+		Integer[] intArray = {10,20,30,40};
+		Character[] charArray = {'a','b','c','d'};
+		printArray(intArray);
+		printArray(charArray);
+		
+	}
+
+}
