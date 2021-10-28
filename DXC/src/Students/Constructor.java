7376ef658e28bec003783d190d07da9c5b2ad645
Jahnavi <Jahnavi@LAPTOP-LAKVACQC>
2021-10-28 16:35:00 +0530

    Constructor

---
+++ DXC/src/Students/Constructor.java
@@ -0,0 +1,70 @@
+package Students;
+import objectclass.util;
+
+
+public class Constructor {
+	
+
+	
+
+
+		String name;
+		int age;
+		
+	/**
+	 * default constructor -- naruto	
+	 */
+		
+	public Constructor () {
+		System.out.println("naruto constructor");
+	
+	}
+	/**
+	 * sasuke constructor
+	 * @param name
+	 */
+		public Constructor (String name) {
+			this.name = name;
+			System.out.println("sasuke constructor");
+		}
+
+	/**
+	 * this is kakashi constructor
+	 * @param name
+	 * @param age
+	 */
+		public Constructor (String name, int age) {
+			System.out.println("kakashi constructor");
+			this.name = name;
+			this.age = util.incrementByTen(age);
+		}
+
+		static String COLLEGE_NAME = "IIT";
+		
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public int getAge() {
+			return age;
+		}
+
+		public void setAge(int age) {
+			this.age = age;
+		}
+		
+	}
+
+
+
+
+
+		
+		
+		
+
+
